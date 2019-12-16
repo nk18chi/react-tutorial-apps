4a7f689b4c2ec550d1285acd89119eb2d839ca97
Naoki Mita <naoki.mita18@gmail.com>
2019-12-15 22:32:57 -0800

    Finish mario plan by lesson 28

---
+++ marioplan/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Dashborad from "./components/dashboard/Dashborad";
+import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashborad} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSummary = ({ project }) => {
+  return (
+    <div className="card z-depth-0 progect-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">{project.title}</span>
+        <p>Posted nby Net Ninja</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
