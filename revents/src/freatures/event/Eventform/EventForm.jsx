edb04e13617c612919c9b7b8a85e8149a61cc96e
Naoki Mita <naoki.mita18@gmail.com>
2019-12-26 18:52:22 -0800

    Finish section 10

--- revents/src/freatures/event/Eventform/EventForm.jsx
+++ revents/src/freatures/event/Eventform/EventForm.jsx
@@ -1,6 +1,8 @@
+/* global google*/
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
 import { createEvent, updateEvent } from "../eventActions";
 import cuid from "cuid";
@@ -14,6 +16,7 @@ import {
   isRequired,
   hasLengthGreaterThan
 } from "revalidate";
+import PlaceInput from "../../../app/common/form/PlaceInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -58,7 +61,13 @@ const category = [
 ];
 
 class EventForm extends Component {
+  state = {
+    cityLatLng: {},
+    venueLatLng: {}
+  };
+
   onFormSubmit = values => {
+    values.venueLatLng = this.state.venueLatLng;
     if (this.props.initialValues.id) {
       this.props.updateEvent(values);
       this.props.history.push(`/events/${this.props.initialValues.id}`);
@@ -72,6 +81,32 @@ class EventForm extends Component {
       this.props.createEvent(newEvent);
       this.props.history.push(`/events/${newEvent.id}`);
     }
+  };
+
+  handleCitySelect = selectedCity => {
+    geocodeByAddress(selectedCity)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          cityLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change("city", selectedCity);
+      });
+  };
+
+  handleVenueSelect = selectedVenue => {
+    geocodeByAddress(selectedVenue)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          venueLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change("venue", selectedVenue);
+      });
   };
 
   render() {
@@ -113,10 +148,22 @@ class EventForm extends Component {
             />
 
             <Header sub color='teal' content='Event Location Details' />
-            <Field name='city' component={TextInput} placeholder='Event City' />
+            <Field
+              name='city'
+              component={PlaceInput}
+              options={{ types: ["(cities)"] }}
+              onSelect={this.handleCitySelect}
+              placeholder='Event City'
+            />
             <Field
               name='venue'
-              component={TextInput}
+              component={PlaceInput}
+              options={{
+                location: new google.maps.LatLng(this.state.cityLatLng),
+                radius: 1000,
+                types: ["establishment"]
+              }}
+              onSelect={this.handleVenueSelect}
               placeholder='Event Venue'
             />
             <Field
--- revents/src/freatures/testarea/TestComponent.jsx
+++ revents/src/freatures/testarea/TestComponent.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { incrementCounter, decrementCounter } from "./testActions";
 import { Button } from "semantic-ui-react";
+import TestPlaceInput from "./TestPlaceInput";
+import SimpleMap from "./SimpleMap";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
 const mapState = state => ({
   data: state.test.data
@@ -13,6 +16,24 @@ const actions = {
 };
 
 class TestComponent extends Component {
+  state = {
+    latlng: {
+      lat: 59.95,
+      lng: 30.33
+    }
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latlng: latLng
+        });
+      })
+      .catch(error => console.error("Error", error));
+  };
+
   render() {
     const { data, incrementCounter, decrementCounter } = this.props;
     return (
@@ -21,6 +42,10 @@ class TestComponent extends Component {
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} positive content='Increment' />
         <Button onClick={decrementCounter} negative content='Decrement' />
+        <br />
+        <br />
+        <TestPlaceInput selectAddress={this.handleSelect} />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     );
   }
