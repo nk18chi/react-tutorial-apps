23f7886165b462fae9db529449713aa4556dc906
Naoki Mita <naoki.mita18@gmail.com>
2019-12-27 23:23:34 -0800

    Finish section 13

--- revents/src/app/reducers/rootReducer.js
+++ revents/src/app/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { reducer as FormReducer } from "redux-form";
+import { reducer as ToastrReducer } from "react-redux-toastr";
 import testReducer from "../../freatures/testarea/testReducer";
 import eventReducer from "../../freatures/event/eventReducer";
 import modalReducer from "../../freatures/modals/modalReducer";
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   events: eventReducer,
   modals: modalReducer,
   auth: authReducer,
-  async: asyncReducer
+  async: asyncReducer,
+  toastr: ToastrReducer
 });
 
 export default rootReducer;
