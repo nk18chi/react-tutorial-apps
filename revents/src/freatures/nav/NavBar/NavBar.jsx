fac12051aaafe58ce781f9b46cb9c6015aa1ec35
Naoki Mita <naoki.mita18@gmail.com>
2019-12-31 17:24:01 -0800

    Stash for challenge 1

--- revents/src/freatures/nav/NavBar/NavBar.jsx
+++ revents/src/freatures/nav/NavBar/NavBar.jsx
@@ -52,7 +52,11 @@ class NavBar extends Component {
             </Fragment>
           )}
           {authenticated ? (
-            <SignedInMenu signOut={this.handleSignOut} profile={profile} />
+            <SignedInMenu
+              auth={auth}
+              signOut={this.handleSignOut}
+              profile={profile}
+            />
           ) : (
             <SignedOutMenu
               signIn={this.handleSignIn}
