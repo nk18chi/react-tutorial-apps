d3cef3aa7be51f159258ac7bf48ca1adea25e0f3
Naoki Mita <naoki.mita18@gmail.com>
2020-01-18 23:28:06 -0800

    Can add news link into firebase

--- hooks-news/src/components/Auth/validateCreateLink.js
+++ hooks-news/src/components/Auth/validateCreateLink.js
@@ -1,1 +1,17 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+  let errors = {};
+
+  if (!values.description) {
+    errors.description = "Description required";
+  } else if (values.description.length < 10) {
+    errors.description = "Description must be at least 10 characters￥";
+  }
+
+  if (!values.url) {
+    errors.url = "URL required";
+  } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+    errors.url = "URL must be valid";
+  }
+
+  return errors;
+}
--- hooks-news/src/firebase/firebase.js
+++ hooks-news/src/firebase/firebase.js
@@ -1,11 +1,13 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import firebaseConfig from "./config";
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
   async register(name, email, password) {
