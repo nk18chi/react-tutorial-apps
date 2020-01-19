40ebe55e36f8951f06b954dd5ffabf5a1c133643
Naoki Mita <naoki.mita18@gmail.com>
2020-01-19 00:44:19 -0800

    Finish CRUD operations for news link

--- hooks-news/src/components/Link/LinkItem.js
+++ hooks-news/src/components/Link/LinkItem.js
@@ -1,7 +1,76 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { getDomain } from "../../utils";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
+import FirebaseContext from "../../firebase/context";
 
-function LinkItem() {
-  return <div>LinkItem</div>;
+function LinkItem({ link, index, showCount, history }) {
+  const { firebase, user } = React.useContext(FirebaseContext);
+
+  function handleVote() {
+    if (!user) {
+      history.pushState("/login");
+    } else {
+      const voteRef = firebase.db.collection("links").doc(link.id);
+      voteRef.get().then(doc => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      });
+    }
+  }
+
+  function handleDeleteLink() {
+    const linkRef = firebase.db.collection("links").doc(link.id);
+    linkRef
+      .delete()
+      .then(() => {
+        console.log(`Document with ID ${link.id} deleted`);
+      })
+      .catch(error => {
+        console.error("Error deleting document:", error);
+      });
+  }
+
+  const postedByAuthUser = user && user.uid === link.postedBy.id;
+
+  return (
+    <div className='flex items-start mt2'>
+      <div className='flex items-center'>
+        {showCount && <span className='grey'>{index}.</span>}
+      </div>
+      <div className='vote-button' onClick={handleVote}>
+        ▲
+      </div>
+      <div className='ml1'>
+        <div>
+          {link.description}{" "}
+          <span className='link'>({getDomain(link.url)})</span>
+        </div>
+        <div className='f6 1h-copy gray'>
+          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {distanceInWordsToNow(link.created)}
+          {" | "}
+          <Link to={`link/${link.id}`}>
+            {link.comments.length > 0
+              ? `${link.comments.length} comments`
+              : "discuss"}
+          </Link>
+          {postedByAuthUser && (
+            <>
+              {" | "}
+              <span className='delete-button' onClick={handleDeleteLink}>
+                delete
+              </span>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
--- hooks-news/src/components/Link/LinkList.js
+++ hooks-news/src/components/Link/LinkList.js
@@ -1,7 +1,38 @@
 import React from "react";
+import FirebaseContext from "../../firebase/context";
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
-  return <div>LinkList</div>;
+  const { firebase } = React.useContext(FirebaseContext);
+  const [links, setLinks] = React.useState([]);
+
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() };
+    });
+    setLinks(links);
+  }
+
+  return (
+    <div>
+      {links.map((link, index) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + 1}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
